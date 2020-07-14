2a2470c46c193c09b6546f8fa31b38dab0517143
Sinayra <nayra@hotmail.co.jp>
2020-07-14 12:32:17 -0300

    Hello Vue components
    * with Vue lazy loading package

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Users />
+    <LazyImage data="./404.jpg" />
+    <LazyImage data="./test0.jpg" />
+    <LazyImage data="./test1.jpg" />
+    <LazyImage data="./test2.jpg" />
+    <LazyImage data="./test3.jpg" />
+    <LazyImage data="./test4.jpg" />
+    <LazyImage data="./catdfds.jpg" />
+  </div>
+</template>
+
+<script>
+import Users from "./components/Users.vue"
+import LazyImage from "./components/LazyImage.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Users,
+    LazyImage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LazyImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-lazy-container="{ selector: 'img' }">
+    <img v-bind:data-src="data" />
+    <span v-for="index in 10" :key="index"><br /></span>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueLazyload from "vue-lazyload";
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: "./error.png",
+  loading: "./loading.gif",
+  attempt: 1,
+});
+
+export default {
+  name: "LazyImage",
+  props: ["data"],
+};
+</script>
+
+<style scoped></style>
